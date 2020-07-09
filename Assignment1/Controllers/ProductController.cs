65ae58a4f40773d1b5a91849d4e9a1db2b7c1bcb
Nikita_Gupta <nikita90139747@gmail.com>
2020-07-09 12:50:03 +0530

    Added ProductController, Product, Create and Index

---
+++ Assignment1/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Assignment1.Models;
+
+
+namespace Assignment1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Product> list = new List<Product>();
+        [Route("ProductDetail")]
+        public IActionResult Index()
+        {
+            return View(list);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                list.Add(p);
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+    }
+}
---
+++ Assignment1/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment1.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage =" Product Id Required")]
+        public string Pid { get; set; }
+
+        [Required(ErrorMessage = " Product name Required")]
+        public string Pname { get; set; }
+
+        [Required(ErrorMessage = " Price Required")]
+        public string Price { get; set; }
+
+        [Required(ErrorMessage = " Stock Required")]
+        public int Stock { get; set; }
+
+    }
+}
